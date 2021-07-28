8dbc765b74e2098ff3733de3667decda30c0580d
rupeshtiwari <roopkt@gmail.com>
2021-07-28 06:46:01 -0400

    chore: climbing stairs k jumps

--- src/04-climbing-staircase-atmost-k-jumps.js
+++ src/04-climbing-staircase-atmost-k-jumps.js
@@ -1,21 +1,31 @@
+/**
+ * https://www.youtube.com/watch?v=uNqoQ0sNZCM&list=PL-Jc9J83PIiG8fE6rj9F5a6uyQ5WPdqKy&index=3
+ */
 class ClimbingStairCaseAtmostKsteps {
-  execute(num, k) {
-    if (num === 0 || num === 1) return 1;
-
-
+  execute(jumps) {
+    /**
+     * 1. state array of number of possible jumps
+     * 2. direction - move from smaller to larger problem. So I will move from last to start of the array.
+     * 3. Solve
+     */
+    const length = jumps.length;
+    const dp = new Array(length).fill(0);
+    dp[length] = 1;
+    for (let idx = length - 1; idx >= 0; idx--) {
+      for (let j = 1; j <= jumps[idx] && j + idx < dp.length; j++) {
+        dp[idx] += dp[j + idx];
+      }
+    }
+    return dp;
   }
 }
 
-xdescribe('Climbing Stairs Atmost K steps', () => {
+describe('Climbing Stairs Atmost K steps', () => {
   it('should work #1', () => {
     var start = performance.now();
-    expect(new ClimbingStairCaseAtmostKsteps().execute(4)).toBe(5);
-    var end = performance.now();
-    console.log('Iterative', end - start);
-  });
-  it('should work #2', () => {
-    var start = performance.now();
-    expect(new ClimbingStairCaseAtmostKsteps().execute(3)).toBe(3);
+    expect(
+      new ClimbingStairCaseAtmostKsteps().execute([3, 3, 0, 2, 2, 3])
+    ).toEqual([8, 5, 0, 3, 2, 1, 1]);
     var end = performance.now();
     console.log('Iterative', end - start);
   });
